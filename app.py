55018cca7604c3eb51006c732d77c454e794943a
Chunkit <141215935+Chunkit@users.noreply.github.com>
2023-09-16 21:39:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def viewStudentInfoDetails(stud_id):
 
     return render_template('viewStudentInfoDetails.html', student=result)
 
-@app.route('/editStudentInfoDetails.html/<int:stud_id>')
-def edit_internship(internship_id):
+@app.route('/editStudentInfoDetails/<int:stud_id>')
+def editStudent(stud_id):
 
     statement = "SELECT * FROM Students WHERE stud_id = %s"
     cursor = db_conn.cursor()
@@ -61,6 +61,24 @@ def edit_internship(internship_id):
 
     return render_template('viewStudentInfoDetails.html', intern=result)
 
+@app.route('/updateStudent', methods=['POST'])
+def updateStudent():
+
+    stud_id =  request.form['intern_id']
+    job_title = request.form['job_title']
+    job_desc = request.form['job_description']
+    job_salary = request.form['job_salary']
+    job_location = request.form['job_location']
+    workingDay = request.form['workingDay']
+    workingHour = request.form['workingHour']
+    accommodation = request.form['accommodation']
+
+    statement = "UPDATE Internship SET job_title = %s, job_description = %s, intern_salary = %s, location = %s, workingDay = %s, workingHour = %s, accommodation = %s WHERE intern_id = %s;"
+    cursor = db_conn.cursor()
+    cursor.execute(statement, (job_title, job_desc, job_salary, job_location, workingDay, workingHour, accommodation, intern_id))
+    db_conn.commit()  # Commit the changes to the database
+
+    return redirect("/viewIntern/" + intern_id)
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
